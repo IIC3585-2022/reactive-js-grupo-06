823130794a5078e9db444e3dbe2f16c4c1f217d4
juanpacl111 <juan.pablo.contreras.g@gmail.com>
2022-04-07 11:24:55 -0400

    Agragadas varias funciones

--- app.js
+++ app.js
@@ -1,61 +1,133 @@
-const { fromEvent, scan, Observable, throttleTime } = require("rxjs");
+const {
+	fromEvent,
+	scan,
+	Observable,
+	throttleTime,
+	timer,
+	startWith,
+} = require("rxjs");
 
 fromEvent(document, "DOMContentLoaded").subscribe(() => {
-	/* document.addEventListener('DOMContentLoaded', () => { */
 	const scoreDisplay = document.getElementById("score");
-  const width = 33;
-  const height = 21;
-  let score = 0;
-  const grid = document.querySelector(".grid");
+	const width = 33;
+	const height = 21;
+	let score = 0;
+	const grid = document.querySelector(".grid");
 
-  const layout = [
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,1],
-    [1,0,1,1,1,1,1,1,0,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1,1,1,1,1,1,0,1],
-    [1,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,1],
-    [1,0,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1],
-    [1,0,0,0,3,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,3,0,0,0,1],
-    [1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1],
-    [4,4,4,4,4,4,4,1,0,1,0,0,3,0,0,0,0,0,0,0,3,0,0,1,0,1,2,2,2,2,2,2,2],
-    [1,1,1,1,1,1,1,1,0,1,0,1,1,1,2,2,2,2,2,1,1,1,0,1,0,1,1,1,1,1,1,1,1],
-    [0,0,0,0,0,0,0,0,0,0,0,1,2,2,2,2,2,2,2,2,2,1,0,0,0,0,0,0,0,0,0,0,0],
-    [1,1,1,1,1,1,1,1,0,1,0,1,2,2,2,2,2,2,2,2,2,1,0,1,0,1,1,1,1,1,1,1,1],
-    [0,0,0,0,0,0,0,0,0,0,0,1,2,2,2,2,2,2,2,2,2,1,0,0,0,0,0,0,0,0,0,0,0],
-    [1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,1],
-    [4,4,4,4,4,4,4,1,0,1,0,0,3,0,0,0,0,0,0,0,3,0,0,1,0,1,2,2,2,2,2,2,2],
-    [1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1],
-    [1,0,0,0,3,0,0,0,0,1,4,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,3,0,0,0,1],
-    [1,0,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1],
-    [1,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,1],
-    [1,0,1,1,1,1,1,1,0,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1,1,1,1,1,1,0,1],
-    [1,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-  ];
+	const layout = [
+		[
+			1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+			1, 1, 1, 1, 1, 1, 1, 1,
+		],
+		[
+			1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 3, 0, 0, 0, 1,
+		],
+		[
+			1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0,
+			1, 1, 1, 1, 1, 1, 0, 1,
+		],
+		[
+			1, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+			0, 0, 0, 0, 0, 0, 0, 1,
+		],
+		[
+			1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0,
+			1, 1, 1, 1, 1, 1, 0, 1,
+		],
+		[
+			1, 0, 0, 0, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0,
+			0, 0, 0, 3, 0, 0, 0, 1,
+		],
+		[
+			1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0,
+			1, 1, 1, 1, 1, 1, 1, 1,
+		],
+		[
+			4, 4, 4, 4, 4, 4, 4, 1, 0, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 1, 0,
+			1, 2, 2, 2, 2, 2, 2, 2,
+		],
+		[
+			1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 0, 1, 0,
+			1, 1, 1, 1, 1, 1, 1, 1,
+		],
+		[
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0,
+		],
+		[
+			1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 1, 0,
+			1, 1, 1, 1, 1, 1, 1, 1,
+		],
+		[
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0,
+		],
+		[
+			1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0,
+			1, 1, 1, 1, 1, 1, 1, 1,
+		],
+		[
+			4, 4, 4, 4, 4, 4, 4, 1, 0, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 1, 0,
+			1, 2, 2, 2, 2, 2, 2, 2,
+		],
+		[
+			1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0,
+			1, 1, 1, 1, 1, 1, 1, 1,
+		],
+		[
+			1, 0, 0, 0, 3, 0, 0, 0, 0, 1, 4, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 4, 1, 0,
+			0, 0, 0, 3, 0, 0, 0, 1,
+		],
+		[
+			1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0,
+			1, 1, 1, 1, 1, 1, 0, 1,
+		],
+		[
+			1, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+			0, 0, 0, 0, 0, 0, 0, 1,
+		],
+		[
+			1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0,
+			1, 1, 1, 1, 1, 1, 0, 1,
+		],
+		[
+			1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 3, 0, 0, 0, 1,
+		],
+		[
+			1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+			1, 1, 1, 1, 1, 1, 1, 1,
+		],
+	];
 
-  const squares = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-  ];
-  
+	max_points = 0;
+
+	//Arreglar esto sí o sí
+	const squares = [
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+	];
+
 	function createBoard() {
 		for (let i = 0; i < height; i++) {
 			for (let j = 0; j < width; j++) {
@@ -66,94 +138,189 @@ fromEvent(document, "DOMContentLoaded").subscribe(() => {
 				//add layout to the board
 				if (layout[i][j] === 0) {
 					squares[i][j].classList.add("food-dot");
+					max_points += 1;
 				} else if (layout[i][j] === 1) {
 					squares[i][j].classList.add("wall");
 				} else if (layout[i][j] === 2) {
 					squares[i][j].classList.add("spawn");
 				} else if (layout[i][j] === 3) {
-					squares[i][j].classList.add("super-food");
+					squares[i][j].classList.add("power-up");
+					max_points += 20;
 				}
 			}
 		}
 	}
+
 	createBoard();
-  
-  let pacmanY = 15;
-  let pacmanX = 10;
-  squares[pacmanY][pacmanX].classList.add("pac-man");
 
-  fromEvent(document, "keydown").subscribe((key) => {
-		console.log("recibido");
-		console.log(key);
-		console.log(key.code);
-		console.log(squares[pacmanY][pacmanX]);
-		squares[pacmanY][pacmanX].classList.add("holdmfie");
-		squares[pacmanY][pacmanX].classList.remove("holdmfie");
-		squares[pacmanY][pacmanX].classList.remove("pac-man");
-		console.log(squares[pacmanY][pacmanX]);
-
-		if (key.code === "KeyA" || key.code === "ArrowLeft") {
-			if (
-				pacmanX > 0 &&
-				!squares[pacmanY][pacmanX - 1].classList.contains("wall")
-			) {
-				console.log("ya cachamos");
-				console.log(pacmanX);
-				pacmanX = pacmanX - 1;
-			}
-		} else if (key.code === "KeyW" || key.code === "ArrowUp") {
-			if (
-				pacmanY > 0 &&
-				!squares[pacmanY - 1][pacmanX].classList.contains("wall")
-			) {
-				pacmanY -= 1;
-			}
-		} else if (key.code === "KeyD" || key.code === "ArrowRight") {
-			if (
-				pacmanX < width &&
-				!squares[pacmanY][pacmanX + 1].classList.contains("wall")
-			) {
-				pacmanX += 1;
-			}
-		} else if (key.code === "KeyS" || key.code === "ArrowDown") {
-			if (
-				pacmanY < height &&
-				!squares[pacmanY + 1][pacmanX].classList.contains("wall")
-			) {
-				pacmanY += 1;
-			}
+	class Pacman {
+		constructor(name, initY, initX) {
+			this.name = name;
+			this.y = initY;
+			this.x = initX;
 		}
-		console.log(pacmanY, pacmanX);
-		squares[pacmanY][pacmanX].classList.add("pac-man");
+	}
+
+	morty = new Pacman("morty", 15, 22);
+	rick = new Pacman("rick", 15, 10);
+
+	addToSquare(rick);
+	addToSquare(morty);
+
+	function removeFromSquare(unit) {
+		squares[unit.y][unit.x].classList.remove(unit.name);
+		if (unit.name == "ghost")
+			squares[unit.y][unit.x].classList.remove("scared-ghost");
+	}
+	function addToSquare(unit) {
+		squares[unit.y][unit.x].classList.add(unit.name);
+		if (unit.name == "ghost") {
+			if (unit.scared) squares[unit.y][unit.x].classList.add("scared-ghost");
+		}
+	}
+
+	//Aquí se mueven los personajes
+	fromEvent(document, "keydown").subscribe((key) => {
+		if (key.code === "KeyA") {
+			removeFromSquare(rick);
+			if (rick.x === 0) {
+				rick.x = 32;
+			} else if (
+				rick.x > 0 &&
+				!squares[rick.y][rick.x - 1].classList.contains("wall")
+			) {
+				rick.x = rick.x - 1;
+			}
+
+			addToSquare(rick);
+		} else if (key.code === "KeyW") {
+			removeFromSquare(rick);
+			if (
+				rick.y > 0 &&
+				!squares[rick.y - 1][rick.x].classList.contains("wall")
+			) {
+				rick.y -= 1;
+			}
+			addToSquare(rick);
+		} else if (key.code === "KeyD") {
+			removeFromSquare(rick);
+			if (rick.x === 32) {
+				rick.x = 0;
+			} else if (
+				rick.x < width &&
+				!squares[rick.y][rick.x + 1].classList.contains("wall")
+			) {
+				rick.x += 1;
+			}
+			addToSquare(rick);
+		} else if (key.code === "KeyS") {
+			removeFromSquare(rick);
+			if (
+				!(
+					rick.y === 7 &&
+					(rick.x === 14 ||
+						rick.x === 15 ||
+						rick.x === 16 ||
+						rick.x === 17 ||
+						rick.x === 18)
+				) &&
+				rick.y < height &&
+				!squares[rick.y + 1][rick.x].classList.contains("wall")
+			) {
+				rick.y += 1;
+			}
+			addToSquare(rick);
+		} else if (key.code === "ArrowLeft") {
+			removeFromSquare(morty);
+			if (morty.x === 0) {
+				morty.x = 32;
+			} else if (
+				morty.x > 0 &&
+				!squares[morty.y][morty.x - 1].classList.contains("wall")
+			) {
+				morty.x = morty.x - 1;
+			}
+			addToSquare(morty);
+		} else if (key.code === "ArrowUp") {
+			removeFromSquare(morty);
+			if (
+				morty.y > 0 &&
+				!squares[morty.y - 1][morty.x].classList.contains("wall")
+			) {
+				morty.y -= 1;
+			}
+			addToSquare(morty);
+		} else if (key.code === "ArrowRight") {
+			removeFromSquare(morty);
+			if (morty.x === 32) {
+				morty.x = 0;
+			} else if (
+				morty.x < width &&
+				!squares[morty.y][morty.x + 1].classList.contains("wall")
+			) {
+				morty.x += 1;
+			}
+			addToSquare(morty);
+		} else if (key.code === "ArrowDown") {
+			removeFromSquare(morty);
+			if (
+				!(
+					rick.y === 7 &&
+					(rick.x === 14 ||
+						rick.x === 15 ||
+						rick.x === 16 ||
+						rick.x === 17 ||
+						rick.x === 18)
+				) &&
+				morty.y < height &&
+				!squares[morty.y + 1][morty.x].classList.contains("wall")
+			) {
+				morty.y += 1;
+			}
+			addToSquare(morty);
+		}
+
 		foodEaten();
+		powerUpEaten();
+		checkLose();
+		checkWin();
 	});
 
 	function foodEaten() {
-		if (squares[pacmanY][pacmanX].classList.contains("food-dot")) {
+		if (squares[rick.y][rick.x].classList.contains("food-dot")) {
 			score++;
 			scoreDisplay.innerHTML = score;
-			squares[pacmanY][pacmanX].classList.remove("food-dot");
+			squares[rick.y][rick.x].classList.remove("food-dot");
+		}
+
+		if (squares[morty.y][morty.x].classList.contains("food-dot")) {
+			score++;
+			scoreDisplay.innerHTML = score;
+			squares[morty.y][morty.x].classList.remove("food-dot");
 		}
 	}
 
 	class Ghost {
-		constructor(id, initY, initX, speed) {
-			this.id = id;
+		constructor(initY, initX, speed) {
+			this.initY = initY;
+			this.initX = initX;
 			this.y = initY;
 			this.x = initX;
+			this.name = "ghost";
 			this.speed = speed;
 			this.current_direction = "up";
+			this.scared = false;
 		}
 	}
 
-  const ghostArray = [
-    new Ghost(0, 10, 14, 400),
-    new Ghost(1, 10, 15, 500),
-    new Ghost(2, 10, 16, 600),
-    new Ghost(3, 10, 17, 700),
-  ];
+	const ghostArray = [
+		new Ghost(10, 14, 500),
+		new Ghost(10, 15, 1200),
+		new Ghost(10, 16, 1800),
+		new Ghost(10, 17, 4000),
+	];
 
-  ghostArray.forEach((ghost) =>
+	ghostArray.forEach((ghost) =>
 		squares[ghost.y][ghost.x].classList.add("ghost")
 	);
 
@@ -174,29 +341,29 @@ fromEvent(document, "DOMContentLoaded").subscribe(() => {
 		right_square = squares[ghost.y][ghost.x + 1];
 		if (validGhostSquare(right_square)) valid_moves.push("right");
 
-		console.log(valid_moves);
 		return valid_moves;
 	}
 
 	function moveGhost(ghost) {
-		squares[ghost.y][ghost.x].classList.remove("ghost");
+		removeFromSquare(ghost);
+
 		if (ghost.direction === "up") {
-			console.log("up");
 			ghost.y -= 1;
-			squares[ghost.y][ghost.x].classList.add("ghost");
+			addToSquare(ghost);
 		} else if (ghost.direction === "down") {
-			console.log("down");
 			ghost.y += 1;
-			squares[ghost.y][ghost.x].classList.add("ghost");
+			addToSquare(ghost);
 		} else if (ghost.direction === "right") {
-			console.log("right");
 			ghost.x += 1;
-			squares[ghost.y][ghost.x].classList.add("ghost");
+			addToSquare(ghost);
 		} else if (ghost.direction === "left") {
-			console.log("left");
 			ghost.x -= 1;
-			squares[ghost.y][ghost.x].classList.add("ghost");
+			addToSquare(ghost);
 		}
+		/* 
+		if (ghost.scared) {
+			squares[ghost.y][ghost.x].classList.add("scared-ghost");
+		} */
 
 		return ghost;
 	}
@@ -211,12 +378,83 @@ fromEvent(document, "DOMContentLoaded").subscribe(() => {
 			} else {
 				direction = valid_moves[Math.floor(Math.random() * valid_moves.length)];
 				ghost.direction = direction;
-				console.log("cambio la direccion a:", direction);
+
 				ghost = moveGhost(ghost);
-				console.log(ghost.y, ghost.x);
 			}
 		}, ghost.speed);
 	}
 
 	ghostArray.forEach((ghost) => movingGhost(ghost));
+
+	function scaredGhost(ghost) {
+		ghost.scared = true;
+		addToSquare(ghost);
+	}
+
+	function unScareGhosts() {
+		ghostArray.forEach((ghost) => {
+			ghost.scared = false;
+			squares[ghost.y][ghost.x].classList.remove("scared-ghost");
+		});
+	}
+
+	let timer;
+
+	const scareGhosts = () => {
+		timer = window.setTimeout(() => {
+			unScareGhosts();
+		}, 6000);
+	};
+
+	function powerUpEaten() {
+		if (squares[rick.y][rick.x].classList.contains("power-up")) {
+			score += 20;
+			scoreDisplay.innerHTML = score;
+			squares[rick.y][rick.x].classList.remove("power-up");
+			ghostArray.forEach((ghost) => scaredGhost(ghost));
+			clearTimeout(timer);
+			scareGhosts();
+		} else if (squares[morty.y][morty.x].classList.contains("power-up")) {
+			score += 20;
+			scoreDisplay.innerHTML = score;
+			squares[morty.y][morty.x].classList.remove("power-up");
+			ghostArray.forEach((ghost) => scaredGhost(ghost));
+			clearTimeout(timer);
+			scareGhosts();
+		}
+	}
+
+	function checkWin() {
+		console.log(score);
+		console.log(max_points);
+		if (score === max_points) {
+			console.log("you win");
+		}
+	}
+
+	function checkLose() {
+		lost = false;
+		rick_square = squares[rick.y][rick.x];
+		morty_square = squares[morty.y][morty.x];
+
+		if (rick_square === morty_square) lost = true;
+		if (
+			rick_square.classList.contains("ghost") &&
+			!rick_square.classList.contains("scared-ghost")
+		)
+			lost = true;
+		if (
+			morty_square.classList.contains("ghost") &&
+			!morty_square.classList.contains("scared-ghost")
+		)
+			lost = true;
+
+		if (lost) {
+			console.log("PERDISTE");
+			clearTimeout(timer);
+			setTimeout(function () {
+				alert("You have Lost!");
+			}, 5);
+		}
+	}
 });
